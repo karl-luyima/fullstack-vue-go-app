342665dbba92aeaff1e2688b887f664de0df9ac8
Luyima Karl Kulembera <130744306+karl-luyima@users.noreply.github.com>
2026-08-26 15:11:49 +0300

    frontend/src folders

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-slate-100">
+    <h1 class="text-3xl font-bold text-blue-600">Tailwind is working</h1>
+  </div>
+</template>
